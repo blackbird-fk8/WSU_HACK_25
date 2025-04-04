72d3f44926cbca2f74b0d0ec4879e4edf718eb16
AndyBogy <andrew.bogush@wsu.edu>
2025-04-04 12:18:38 -0700

    messing around

--- hack2025.py
+++ hack2025.py
@@ -1,2 +1,7 @@
-print("Hello World")
-print("This is a test")
+# test
+input = input("Enter aything, other than the value of 2: ")
+if input == "2":
+    print("You need to take a dump")
+else:
+    print(f"You entered: {input}") 
+   
